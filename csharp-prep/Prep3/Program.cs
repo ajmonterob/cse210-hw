9bdf6d0c78c2bb5d20131fd771eea790e965dbc0
Andres <andres.josue.montero@gmail.com>
2023-11-13 21:45:08 -0600

    Prep Final

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Step 1: Generate a random number from 1 to 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        // Step 2: Add a loop to keep playing until the guess is correct
+        bool guessedCorrectly = false;
+
+        while (!guessedCorrectly)
+        {
+            // Step 3: Ask the user for a guess
+            Console.Write("What is your guess? ");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            // Step 4: Determine if the user needs to guess higher, lower, or if they guessed it
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                guessedCorrectly = true; // Exit the loop
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+
+            if (input == "0")
+                break; 
+
+            if (int.TryParse(input, out int number))
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+
+        int sum = numbers.Sum();
+
+        double average = numbers.Average();
+
+        int maxNumber = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
